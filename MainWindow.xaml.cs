15f12b0d45b21e98eb51a04004de4a836f81412d
Feer <meydon86@gmail.com>
2023-05-05 20:16:09 +0700

    FirstCommit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using Calc.Utils;
+
+namespace Calc
+{
+    //Дима не смотри сюда тебе тут не рады
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
